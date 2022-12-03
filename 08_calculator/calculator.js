202cf6ed58862c738b8e181776863dcf563c1683
JT <jerylltan@gmail.com>
2022-12-03 23:46:22 +0800

    completed 08 and 09

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+	return nums.reduce((total, cur) => cur + total, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+  return nums.reduce((total, cur) => cur * total);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a**b
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  res = 1
+  for(i = a; i > 0; i--) {
+    res *= i;
+  }
+  return res;
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,23 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    const words = Array.from(word).map(w => w.toLowerCase());
+    let pureWords = words.filter(pureWord => {
+        if (pureWord.charCodeAt(0) >= 97 && pureWord.charCodeAt(0) <= 122) {
+            return pureWord;
+        };
+    });
 
+    console.log(pureWords)
+    l = 0;
+    r = pureWords.length-1;
+
+    while (l < r){
+        if (pureWords[l] !== pureWords[r]) {
+            return false;
+        };
+        l++;
+        r--;
+    };
+    return true;
 };
 
 // Do not edit below this line
